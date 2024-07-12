a9db8581bce0846c2672883b3ac3c2e5abc132b3
Susnn90 <susann-albrecht@gmx.de>
2024-07-12 12:42:41 +0200

    .

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main (int argc, char **argv, char **envp)
+{
+    char    *input;
+
+    // input checking argc und argv
+    if (argc != 1)
+    {
+        printf("Wrong input: usage > ./minishell");
+        return (0);
+    }
+
+    while (1)
+    {
+        input = readline("hamstershell > ");
+    }
+    
+
+    //show prompt hamstershell > 
+
+    //input history
+    
+    //input checking -  lexing token checking
+    return(0);
+}
